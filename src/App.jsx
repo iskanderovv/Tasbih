c7d528c39be8895acac11fc6493f154f4d570a14
Akbar Iskanderov <iskanderovofficial@gmail.com>
2024-06-21 09:42:43 +0500

    finished project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Tasbih from './components/tasbih/Tasbih'
+
+function App() {
+
+  return (
+    <>
+      <Tasbih />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/tasbih/Tasbih.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import './Tasbih.scss';
+import countAudio from '../../audio/count.wav';
+import vibrationAudio from '../../audio/vibration.mp3';
+
+const Tasbih = () => {
+    const [count, setCount] = useState(0);
+    const [total, setTotal] = useState(0);
+    const [audioCount] = useState(new Audio(countAudio));
+    const [audioVibration] = useState(new Audio(vibrationAudio))
+
+    const playAudioCount = () => {
+        audioCount.play();
+    }
+
+    const playAudioVibration = () => {
+        audioVibration.play();
+    }
+
+    const handleCount = () => {
+        setCount(prevCount => prevCount + 1);
+        setTotal(prevCount => prevCount + 1);
+        playAudioCount();
+
+        if (count === 32) {
+            playAudioVibration();
+        }
+        if (count === 33) {
+            setCount(0);
+        }
+    }
+
+    const handleReset = () => {
+        setCount(0);
+        setTotal(0);
+    }
+
+    return (
+        <div className='tasbih'>
+            <div className='tasbih__content'>
+                <div className='tasbih-content__tools'>
+                    <p className='tasbih-content-tools__total'>Total: {total}</p>
+                    <button className='tasbih-content-tools__reset' onClick={handleReset}>Reset</button>
+                </div>
+                <h1 className='tasbih-content__counter'>{count}</h1>
+                <div className='tasbih-content__button' onClick={handleCount}>
+                    <i className="bi bi-chevron-up"></i>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Tasbih;
